939b45be44cb2d184229e12db86b9f8cea91ecae
piensosurbanoweb <piensosurbanoweb@gmail.com>
2026-01-12 12:36:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -57,12 +57,8 @@ app.use(express.json()); // Para parsear JSON en POST
 
 // 🔑 CONFIGURACIÓN DE CONEXIÓN A POSTGRESQL
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 // Helper para obtener día de la semana (UTC) en minúsculas y sin acentos
